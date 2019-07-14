a8bfbf554b08d81f736efda4a1d45fec5296f392
grglym <1099331915@qq.com>
2019-07-15 00:16:13 +0800

    服务端多进程连接

--- socket/server.c
+++ socket/server.c
@@ -15,6 +15,24 @@
 		exit(EXIT_FAILURE); \
 	} while(0)
 
+void do_service(int conn)
+{
+	char recvbuf[1024]={0};
+        while(1)
+        {
+                memset(recvbuf, 0, sizeof(recvbuf));
+        	
+		
+		int ret = read(conn, recvbuf, sizeof(recvbuf));
+		if(ret == 0)
+		{
+			printf("client close\n");
+			break;
+		}
+		fputs(recvbuf, stdout);
+                write(conn, recvbuf, ret);	
+	}
+}
 int main(void)
 {
 	int listenfd;
@@ -47,27 +65,30 @@ int main(void)
 	struct sockaddr_in peeraddr;
 	socklen_t peerlen = sizeof(peeraddr);
 	int conn;	
-	if ((conn = accept(listenfd, (struct sockaddr*) &peeraddr, &peerlen)) < 0)
-	{
-		ERR_EXIT("accept");
-	}
 	
-	printf("ip=%s port=%d\n", inet_ntoa(peeraddr.sin_addr), ntohs(peeraddr.sin_port));
-	
-	char recvbuf[1024]={0};
 	while(1)
 	{
-		memset(recvbuf, 0, sizeof(recvbuf));
-		int ret = read(conn, recvbuf, sizeof(recvbuf));
-		//fputs(ret, stdout);
-		fputs(recvbuf, stdout);
-		write(conn, recvbuf, ret);
-	}
-
-	close(conn);
-	close(listen);
+		if ((conn = accept(listenfd, (struct sockaddr*) &peeraddr, &peerlen)) < 0)
+		{
+			ERR_EXIT("accept");
+		}
+	
+		printf("ip=%s port=%d\n", inet_ntoa(peeraddr.sin_addr), ntohs(peeraddr.sin_port));
+		
+		int pid = fork();
+		if(pid == -1)
+		{
+			ERR_EXIT("fork");
+		}
+		if(pid == 0)
+		{
+			close(listenfd);
+			do_service(conn);
+			exit(EXIT_SUCCESS);
+		}
+		else {
+			close(conn);
+		}
+	} 
 	return 0;
-
-
-
 }
