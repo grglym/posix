552ce834ed3c57bcea57a6ff64f19c1d87f3b52f
grglym <1099331915@qq.com>
2019-08-01 00:44:18 +0800

    msgqueue modified

---
+++ .vimrc
@@ -0,0 +1,40 @@
+set fileencoding=utf-8
+set fileencodings=utf-8,gb2312,gb18030,latin1
+set termencoding=utf-8
+set encoding=utf-8
+
+syntax on
+
+color evening
+
+filetype on
+
+filetype plugin on
+
+
+set number
+
+set cc=64
+
+set cursorline
+
+set tabstop=4
+
+set softtabstop=4
+
+set shiftwidth=4 
+
+set autoindent
+
+set smartindent
+
+set cindent
+
+set expandtab
+
+inoremap jj <Esc>
+
+imap { {<CR>}<Esc>kA<CR>
+
+
+
--- msgqueue/server.c
+++ msgqueue/server.c
@@ -16,6 +16,21 @@ int main()
 	scanf("%d %d",&msg.index, &msg.op);
         sendMsgQueue(msgid, SERVER_TYPE, msg);
         printf("send done, wait recv...\n");
+    
+	int status,i;
+	for(i=0; i<4; i++)
+	{
+		status=fork();
+		if(status == 0 || status == -1)break;
+	}
+	if(status == -1)
+		exit(EXIT_FAILURE);
+	else if(status == 0)
+	{
+		recvMsgQueue(msgid, CLIENT_TYPE, &msg);
+		printf("%d %d",msg.index, msg.op);
+		exit(0);
+	}
     }
 
     destoryMsgQueue(msgid);
